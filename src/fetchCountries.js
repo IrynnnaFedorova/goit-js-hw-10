02f8744c4da77ad9cee3ed2ce75306da2cf9acfd
IrynnnaFedorova <bulhakovairynnna@gmail.com>
2022-02-13 20:15:50 +0200

    добавляет Base url

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,5 +1,6 @@
+const BASE_URL = 'https://restcountries.com/v3.1';
 function fetchCountries(name) {
-    return fetch(`https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`)
+    return fetch(`${BASE_URL}/name/${name}?fields=name,capital,population,flags,languages`)
         .then(response => {
         if (response.status === 404) {
             throw new Error(response.status);
